4ca68b966371a26013e0a471495862bc463d89cb
cargarg7 <cargarg7@gmail.com>
2023-04-25 08:38:13 +0200

    First validate

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,11 +2,48 @@ import { passwordValidator } from './index';
 
 describe('Password Validator', () => {
 	it('Should get ERROR when null or empty string given', () => {
-		expect(() => passwordValidator()).toThrow('Empty password is not valid');
-		expect(() => passwordValidator('')).toThrow('Empty password is not valid');
+		expect(passwordValidator()).toEqual({ status: false, errorMessages: ['Empty password is not valid'] });
+		expect(passwordValidator('')).toEqual({ status: false, errorMessages: ['Empty password is not valid'] });
 	});
 
 	it('Should get ERROR when length is lower than 6 chars', () => {
-		expect(() => passwordValidator('abcde')).toThrow('Length must be greater or equalo to 6');
+		expect(passwordValidator('1Bc_e')).toEqual({
+			status: false,
+			errorMessages: ['Length must be greater or equalo to 6'],
+		});
+	});
+
+	it('Should get ERROR when there is not at least one numbers', () => {
+		expect(passwordValidator('Abc_ef')).toEqual({
+			status: false,
+			errorMessages: ['Must be contain at least one number'],
+		});
+	});
+
+	it('Should get ERROR when there is not at least one upper case', () => {
+		expect(passwordValidator('1bc_ef')).toEqual({
+			status: false,
+			errorMessages: ['Must be contain at least one upper case'],
+		});
+	});
+
+	it('Should get ERROR when there is not at least one lower case', () => {
+		expect(passwordValidator('1BC_EF')).toEqual({
+			status: false,
+			errorMessages: ['Must be contain at least one lower case'],
+		});
+	});
+
+	it('Should get ERROR when there is not at least one underscore case', () => {
+		expect(passwordValidator('1bcdEF')).toEqual({
+			status: false,
+			errorMessages: ['Must be contain at least one underscore'],
+		});
+	});
+
+	it('Should get validate when everything is ok', () => {
+		expect(passwordValidator('1bc_EF')).toEqual({
+			status: true,
+		});
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,49 @@
 const MIN_LENGTH = 6;
+const AT_LEAST_ONE_NUMBER_REGEX = /(.*[0-9].*)/;
+const AT_LEAST_ONE_UPPER_CASE_REGEX = /(.*[A-Z].*)/;
+const AT_LEAST_ONE_LOWER_CASE_REGEX = /(.*[a-z].*)/;
+const AT_LEAST_ONE_UNDERSCORE_REGEX = /(.*_.*)/;
 
-export function passwordValidator(payload?: string) {
-	if (!payload) throw new Error('Empty password is not valid');
-	if (payload.length < MIN_LENGTH) throw new Error('Length must be greater or equalo to 6');
+type PasswordValidatorResponse = {
+	status: boolean;
+	errorMessages?: string[];
+};
+
+export function passwordValidator(payload?: string): PasswordValidatorResponse {
+	const messages: string[] = [];
+	// Main Validation
+	if (!payload) {
+		return {
+			status: false,
+			errorMessages: ['Empty password is not valid'],
+		};
+	}
+
+	// Rest of validations
+	if (payload.length < MIN_LENGTH) {
+		messages.push('Length must be greater or equalo to 6');
+	}
+	if (!AT_LEAST_ONE_NUMBER_REGEX.test(payload)) {
+		messages.push('Must be contain at least one number');
+	}
+	if (!AT_LEAST_ONE_UPPER_CASE_REGEX.test(payload)) {
+		messages.push('Must be contain at least one upper case');
+	}
+	if (!AT_LEAST_ONE_LOWER_CASE_REGEX.test(payload)) {
+		messages.push('Must be contain at least one lower case');
+	}
+	if (!AT_LEAST_ONE_UNDERSCORE_REGEX.test(payload)) {
+		messages.push('Must be contain at least one underscore');
+	}
+
+	if (messages.length) {
+		return {
+			status: false,
+			errorMessages: messages,
+		};
+	}
+
+	return {
+		status: true,
+	};
 }
